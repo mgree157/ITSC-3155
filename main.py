9bf14a16dbd04e5a6427c442cc8c208d3ee991b0
mgree157 <mgree157@uncc.edu>
2024-06-13 20:02:01 -0400

    feat: handles user payment input and accounting

--- main.py
+++ main.py
@@ -59,6 +59,12 @@ class SandwichMachine:
     def process_coins(self):
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
+        print("Please insert coins.")
+        num_dollars = int(input("How many dollars are you inserting? "))
+        num_half_dollars = int(input("How many half dollars are you inserting? "))
+        num_quarters = int(input("How many quarters are you inserting? "))
+        num_nickles = int(input("How many nickles are you inserting? "))
+        return num_dollars + num_half_dollars * .5 + num_quarters * .25 + num_nickles * .05
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
@@ -79,4 +85,5 @@ elif command == "report":
     print("cheese: " + str(subway.machine_resources["cheese"]) + " slice(s)")
 else:
     if subway.check_resources(recipes[command]["ingredients"]):
-        print("making sandwich")
+        payment = subway.process_coins()
+
